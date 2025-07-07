2f13d04152d87627890b0ffd30d605c9a346c0cb
CodeWithFadii <fahadali@solinovation.com>
2025-07-08 01:16:05 +0500

    Refactor Settings class in config.py to improve readability and maintainability

--- app/config.py
+++ app/config.py
@@ -7,8 +7,12 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expires_days: int
 
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+
     class Config:
         env_file = ".env"
 
 
-settings = Settings()  # type: ignore
+settings = Settings() # type: ignore
